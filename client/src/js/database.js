c4d79f3dff812713fe8cd7df5aa004c37c4ef2ce
Nicolai Rueckert <nrueckert@Nicolais-MacBook-Pro.local>
2023-12-03 21:52:53 -0600

    Added Database Logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Saving to the Database')
+  const jateDB = await openDB('jate', 1)
+  const tran = jateDB.transaction('jate', 'readwrite')
+  const store = tran.objectStore('jate')
+  const request = store.put({id: 1, value: content})
+  const result = await request;
+  console.log(`Saved to the Database ${result.value}`)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+const jateDB = await openDB('jate', 1)
+const tran = jateDB.transaction('jate', 'readonly')
+const store = tran.objectStore('jate')
+const request = store.get(1)
+const result = await request
+if(result){console.log(`Data found ${result.value}`)}
+else{console.log('No Data Found')}
 
 initdb();
